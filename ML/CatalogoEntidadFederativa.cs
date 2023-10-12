9a778679e2488d875398dbe0d1d6fe5c741b2997
MSandoval00 <msandovalg00@gmail.com>
2023-10-12 17:35:01 -0600

    Cambios ML

---
+++ ML/CatalogoEntidadFederativa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class CatalogoEntidadFederativa
+    {
+        public int IdCatalogoEntidadFederativa { get; set; }
+        public string Estado { get; set; }
+        public List<object> CatalogosEntidadFederativas { get; set; }
+    }
+}
